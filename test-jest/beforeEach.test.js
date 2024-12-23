debc3e6bd83efa2a5b29432878b8af64280a6c51
Takahito Urai <uraitakahito@gmail.com>
2024-12-23 18:51:23 +0900

    Refactor test strings to use single quotes for consistency

--- test-jest/beforeEach.test.js
+++ test-jest/beforeEach.test.js
@@ -1,14 +1,14 @@
-describe("id test", () => {
-  let mockObj = { id: "123" };
+describe('id test', () => {
+  let mockObj = { id: '123' };
 
   beforeEach(() => {
-    mockObj = { id: "234" };
+    mockObj = { id: '234' };
   });
 
-  describe("sample 1", () => {
-    it("has valid id", () => {
+  describe('sample 1', () => {
+    it('has valid id', () => {
       expect.assertions(1);
-      expect(mockObj.id).toBe("234");
+      expect(mockObj.id).toBe('234');
     });
   });
 });
